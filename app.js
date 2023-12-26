9fd67bfd78a786cb34b0e310a5f20098ccb105fc
sami561 <77441338+sami561@users.noreply.github.com>
2023-12-26 22:18:16 +0100

    testing api

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 3000;
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/test", (req, res) => res.send("Hello Test!"));
-app.get("/sami",(req, res) => res.send("Hello from sami !"))
+app.get("/sami",(req, res) => res.send("Hello from sami !"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
